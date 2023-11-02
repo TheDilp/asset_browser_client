3ac14901e5a4b9347c12b0da9963a24e283dc8f6
Mihailo Dilparic <mihailo@salestrekker.com>
2023-11-02 21:12:29 +0100

    chore: change target

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import netlify from "@astrojs/netlify/functions";
-
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
   integrations: [tailwind()],
   redirects: {
-    "/": "/dnd/"
+    "/": "/dnd/",
   },
-  adapter: netlify()
 });
