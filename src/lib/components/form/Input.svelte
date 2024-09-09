65a2e52409d2bee07983b23802cbcfa19a916ffc
Mihailo <mihailo.dilparic@gmail.com>
2024-09-09 10:06:10 +0200

    feat: initial input component

---
+++ src/lib/components/form/Input.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { HTMLInputTypeAttribute } from 'svelte/elements';
+
+	export let title = '';
+	export let value = '';
+	export let type: HTMLInputTypeAttribute = 'text';
+</script>
+
+<label {title}>
+	<span class="text-xs">{title}</span>
+	<input
+		{value}
+		{type}
+		class="px-2 rounded h-8 w-full bg-zinc-800 text-white focus:outline-0 focus:ring-2 focus:border-0 ring-offset-0"
+	/>
+</label>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,3 +1,14 @@
+<script>
+	import Input from '$lib/components/form/Input.svelte';
+	let username = 'test';
+	let password = '';
+</script>
+
 <div class="flex h-full items-center justify-center">
-	<div class="w-full h-full lg:w-64 lg:h-64 xl:w-96 xl:h-96 rounded shadow bg-zinc-900"></div>
+	<div
+		class="w-full h-full lg:w-64 lg:h-64 xl:w-96 xl:h-96 rounded shadow bg-zinc-900 flex flex-col gap-y-2 p-4"
+	>
+		<Input bind:value={username} title="Username" />
+		<Input bind:value={password} title="Password" type="password" />
+	</div>
 </div>
