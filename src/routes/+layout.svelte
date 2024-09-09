19f268d3884c7503c9bba2a71369d6a4a29e5783
Mihailo <mihailo.dilparic@gmail.com>
2024-09-09 09:47:19 +0200

    feat: init commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="w-screen h-screen bg-zinc-950 text-white p-4"><slot /></div>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,3 @@
+<div class="flex h-full items-center justify-center">
+	<div class="w-full h-full lg:w-64 lg:h-64 xl:w-96 xl:h-96 rounded shadow bg-zinc-900"></div>
+</div>
