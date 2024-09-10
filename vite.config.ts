2cd2e6b96895a313079b2223bfcee0ee2bb300d8
Mihailo <mihailo.dilparic@gmail.com>
2024-09-10 17:23:10 +0200

    chore: attempt import fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	build: {
+		rollupOptions: {
+			external: ['/app/src/lib/server/util/db.ts']
+		}
+	}
 });
