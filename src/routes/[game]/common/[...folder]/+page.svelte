566f6b543927ae401314280a29532726868496a6
Mihailo <mihailo.dilparic@gmail.com>
2025-01-21 21:12:00 +0100

    chore: hide pagination in folders page

--- src/routes/[game]/common/[...folder]/+page.svelte
+++ src/routes/[game]/common/[...folder]/+page.svelte
@@ -139,52 +139,58 @@
 			</div>
 			<PreviewTable data={data.data || []} {preview} />
 		</div>
-		<div class="w-full flex items-center justify-between">
-			<div class="flex gap-x-2 items-center">
-				<select
-					on:change={selectPage}
-					value={pageNumber}
-					class=" bg-zinc-800 rounded-md text-lg px-4"
-				>
-					{#each Array.from(Array(data.pages).keys()) as page}
-						<option value={page + 1}>Page {page + 1}</option>
-					{/each}
-				</select>
-				<select on:change={selectCount} value={count} class=" bg-zinc-800 rounded-md text-lg px-4">
-					<option value={10}>Items: 10</option>
-					<option value={20}>Items: 20</option>
-					<option value={30}>Items: 30</option>
-					<option value={40}>Items: 40</option>
-					<option value={50}>Items: 50</option>
-					<option value={60}>Items: 60</option>
-					<option value={70}>Items: 70</option>
-					<option value={80}>Items: 80</option>
-					<option value={90}>Items: 90</option>
-					<option value={100}>Items: 100</option>
-				</select>
-			</div>
-			<div class="w-full flex items-center justify-end gap-x-1 py-2">
-				<div class="w-8 h-8">
-					<a href={pageNumber <= 1 ? '#' : `/assets/${$page.params.id}/${pageNumber - 1}`}>
-						<Button
-							variant="info"
-							icon="ph:caret-left"
-							onClick={undefined}
-							disabled={pageNumber === 1}
-						/>
-					</a>
+		{#if !$page.params.folder}
+			<div class="w-full flex items-center justify-between">
+				<div class="flex gap-x-2 items-center">
+					<select
+						on:change={selectPage}
+						value={pageNumber}
+						class=" bg-zinc-800 rounded-md text-lg px-4"
+					>
+						{#each Array.from(Array(data.pages).keys()) as page}
+							<option value={page + 1}>Page {page + 1}</option>
+						{/each}
+					</select>
+					<select
+						on:change={selectCount}
+						value={count}
+						class=" bg-zinc-800 rounded-md text-lg px-4"
+					>
+						<option value={10}>Items: 10</option>
+						<option value={20}>Items: 20</option>
+						<option value={30}>Items: 30</option>
+						<option value={40}>Items: 40</option>
+						<option value={50}>Items: 50</option>
+						<option value={60}>Items: 60</option>
+						<option value={70}>Items: 70</option>
+						<option value={80}>Items: 80</option>
+						<option value={90}>Items: 90</option>
+						<option value={100}>Items: 100</option>
+					</select>
 				</div>
-				<div class="w-8 h-8">
-					<a href={`/assets/${$page.params.id}/${pageNumber + 1}`}>
-						<Button
-							variant="info"
-							icon="ph:caret-right"
-							onClick={undefined}
-							disabled={data.data.length === 0 || pageNumber >= data.pages}
-						/>
-					</a>
+				<div class="w-full flex items-center justify-end gap-x-1 py-2">
+					<div class="w-8 h-8">
+						<a href={pageNumber <= 1 ? '#' : `/assets/${$page.params.id}/${pageNumber - 1}`}>
+							<Button
+								variant="info"
+								icon="ph:caret-left"
+								onClick={undefined}
+								disabled={pageNumber === 1}
+							/>
+						</a>
+					</div>
+					<div class="w-8 h-8">
+						<a href={`/assets/${$page.params.id}/${pageNumber + 1}`}>
+							<Button
+								variant="info"
+								icon="ph:caret-right"
+								onClick={undefined}
+								disabled={data.data.length === 0 || pageNumber >= data.pages}
+							/>
+						</a>
+					</div>
 				</div>
 			</div>
-		</div>
+		{/if}
 	{/if}
 </div>
