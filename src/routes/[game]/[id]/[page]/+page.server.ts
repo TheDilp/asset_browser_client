3f0615af91d1f7c538373c728fefc5a87d6926d0
Mihailo Dilparić <mihailo.dilparic@gmail.com>
2026-01-10 14:00:02 +0100

    fix: request

--- src/routes/[game]/[id]/[page]/+page.server.ts
+++ src/routes/[game]/[id]/[page]/+page.server.ts
@@ -18,8 +18,7 @@ export async function load({ params, locals, url }) {
 			sort: `${sort || 'title'}`,
 			requestKey: `${params.id}/${params.page}`,
 			filter: `owner_id = ${type === 'common' ? 'null' : `'${locals?.user?.id}'`} ${titleFilter ? `&& title ~ '${titleFilter}'` : ''} && 
-			(url ~ 'dnd/${params.game}/%.webp' || url ~ 'dnd/${params.game}/%.png' || url ~ 'dnd/${params.game}/%.jpg' || url ~ 'dnd/${params.game}/%.jpeg' || size = 0)
-			&& ((url !~ 'dnd/${params.game}/%/%.%' && size != 0) || (url !~ 'dnd/${params.game}/%/%' && size = 0)) && game_id = '${gameId}'`
+			((url !~ 'dnd/${params.game}/%/%.%' && size != 0) || (url !~ 'dnd/${params.game}/%/%' && size = 0)) && game_id = '${gameId}'`
 		});
 		
 	
